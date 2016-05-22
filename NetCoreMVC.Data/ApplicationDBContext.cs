fdfad1d5fc931bb2b754726596a8ef939bce6019
Clinton Beyerle <berlstone@hotmail.com>
2016-05-22 21:11:53 +0400

    Add project files.

---
+++ NetCoreMVC.Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Data
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ApplicationDBContext
+    {
+        public ApplicationDBContext()
+        {
+        }
+
+        public string datacall()
+        {
+            return "this is called from data Layer";
+        }
+    }
+}
---
+++ NetCoreMVC.Service/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCoreMVC.Data;
+
+namespace NetCoreMVC.Service
+{
+    public class Service
+    {
+        public Service()
+        {
+        }
+        public string datacall()
+        {
+            var obj = new ApplicationDBContext();
+            return obj.datacall();
+        }
+    }
+
+}
